2af9ec08466154d8534f4473e64df6ee5b824f8a
Chris <chris_thompson@bcit.ca>
2021-11-22 11:59:49 -0800

    Hello world

---
+++ README.md
@@ -0,0 +1,9 @@
+# COMP-2522-Lab-07
+
+### Your name:
+
+### Your student number:
+
+#### ** Add any comments for me below this line **
+
+#### ** Add any comments for me above this line **
---
+++ src/ca/bcit/comp2522/labs/lab07/MazeSolver.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.labs.lab07;
+
+/**
+ * Placeholder for your code. Replace me with your own comments.
+ *
+ * @author YOUR NAME
+ * @version DATE
+ */
+public class MazeSolver {
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        System.out.println("Chris your beard is unparalleled in its majesty.");
+    }
+
+}
